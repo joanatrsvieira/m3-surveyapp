b41e99161acd50ca88cebf8602a846dcff383036
joanatrsvieira <89456483+joanatrsvieira@users.noreply.github.com>
2023-03-21 13:08:56 +0000

    Modified files

--- app.py
+++ app.py
@@ -1,5 +1,20 @@
 import csv
 from extract import get_input
+from duplicates import duplicates
+from emptylines import empty
+from capitalize import capital
+from validate import validate
+from output import output
+from outputprint import print_data
 
-filename = "results.csv"
-output = get_input(filename)
+def input(filename):
+    data = get_input(filename)
+    data=duplicates(data)
+    data=empty(data)
+    data=capital(data)
+    data=validate(data)
+    output(data)
+    print('./clean_results.csv')
+    return(data)
+
+data=input('./results.csv')
